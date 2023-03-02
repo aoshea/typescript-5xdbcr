99f74fd296085480881efe5e496eae2c02635abb
aoshea <aoshea@users.noreply.github.com>
2023-03-02 10:25:06 +0000

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,13 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>Fullscreen test</h1>`;
+
+const svgDiv: SVGElement = document.querySelector('svg');
+
+window.addEventListener('resize', layout);
+layout();
+
+function layout() {
+  svgDiv.setAttribute('width', '100%');
+  svgDiv.setAttribute('min-height', window.innerHeight + 'px');
+}
