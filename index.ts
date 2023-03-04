7210a871bedc73e7529ae0d0a6abdd297bf66d78
aoshea <aoshea@users.noreply.github.com>
2023-03-04 14:01:49 +0000

    1 file modified

--- index.ts
+++ index.ts
@@ -1,15 +1,63 @@
 // Import stylesheets
 import './style.css';
 
+// physics constants
+const FRICTION = 0.975;
+const REST = 24;
+
 // point class
 function Point(x, y) {
   this.x = x;
   this.y = y;
+  this.ax = 0;
+  this.ay = 0;
 }
 
 Point.prototype.update = function () {
-  this.x = this.x + 1;
-  this.y = this.y + 1;
+  this.ax *= FRICTION;
+  this.ay *= FRICTION;
+  this.x = this.x + this.ax;
+  this.y = this.y + this.ay;
+};
+
+Point.prototype.clone = function () {
+  return new Point(this.x, this.y);
+};
+
+// constraint class
+function Constraint(rest, a, b) {
+  this.rest = rest;
+  this.a = a;
+  if (typeof b === 'undefined' || typeof b === null) {
+    this.b = a.clone();
+    this.pin = true;
+  } else {
+    this.b = b;
+  }
+}
+
+Constraint.prototype.update = function () {
+  let dx = this.a.x - this.b.x;
+  let dy = this.a.y - this.b.y;
+  let len = Math.sqrt(dx * dx + dy * dy);
+  if (len === 0) {
+    len += 0.001;
+  }
+  let dist = 1 * (this.rest - len) * 0.5 * -1;
+  let ddx = this.b.x + dx * 0.5;
+  let ddy = this.b.y + dy * 0.5;
+  dx /= len;
+  dy /= len;
+  if (!this.pin) {
+    this.b.x = ddx + dx * 0.5 * this.rest * -1;
+    this.b.y = ddy + dy * 0.5 * this.rest * -1;
+    this.b.ax = this.b.ax + dx * dist;
+    this.b.ay = this.b.ay + dy * dist;
+  }
+  this.a.x = ddx + dx * 0.5 * this.rest;
+  this.a.y = ddy + dy * 0.5 * this.rest;
+  this.a.ax = this.a.ax + dx * -dist;
+  this.a.ay = this.a.ay + dy * -dist;
 };
 
 // blob class
@@ -22,6 +70,8 @@ Blob.prototype.update = function () {
   this.p.update();
 };
 
+// constraints
+const constraints = [];
 // letter blobs
 const blobs = [];
 // letter blob els
@@ -49,7 +99,18 @@ function layout() {
 function init() {
   // create letter blobs
   for (let i = 0; i < 8; ++i) {
-    blobs.push(new Blob(new Point(0, 0), 'A'));
+    const cx = 50;
+    const cy = 50;
+    const angle = (Math.PI / 4) * i;
+    const radius = 10;
+    const x = cx + Math.sin(angle) * radius;
+    const y = cy + Math.cos(angle) * radius;
+    blobs.push(new Blob(new Point(x, y), 'A'));
+    if (i > 0) {
+      // create constraint
+      const constraint = new Constraint(REST, blobs[i - 1].p, blobs[i].p);
+      constraints.push(constraint);
+    }
   }
   for (let i = 0; i < 8; ++i) {
     const blob_el = document.querySelector('g#b-' + i);
@@ -61,7 +122,7 @@ function init() {
 
 // loop
 function gameloop() {
-  // window.requestAnimationFrame(gameloop);
+  window.requestAnimationFrame(gameloop);
 
   update();
   draw();
@@ -70,9 +131,10 @@ function gameloop() {
 // update game
 function update() {
   for (let i = 0; i < blobs.length; ++i) {
-    console.log(blobs[i]);
-    debugger;
     blobs[i].update();
+  }
+  for (let i = 0; i < constraints.length; ++i) {
+    constraints[i].update();
   }
 }
 
